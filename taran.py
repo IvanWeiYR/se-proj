314177ead536f28541302af6b7af1209e19d2e75
IvanWeiYR <wyrivanngai@gmail.com>
2017-11-15 18:06:23 -0800

    quickSort in python, haven't tested correctness, supposed to be correct

--- taran.py
+++ taran.py
@@ -1,2 +1,22 @@
-print "abd"
-print "good"
+from random import randint
+
+def main():
+	a = randint(0,9)
+	b = randint(0,9)
+	c = max(a,b)
+	print c
+	if c > 5 & c <9:
+		print "yes"
+	else: 
+		print "no"
+		print "shit"
+		print "yea"
+
+def max(a,b):
+	if a>b:
+		return a
+	else:
+		return b
+
+
+main()
