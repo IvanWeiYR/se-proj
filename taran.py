2a668df074aed350802115edaa6e4af448b0ca4d
IvanWeiYR <wyrivanngai@gmail.com>
2017-12-08 02:44:08 -0800

    finished taran

--- taran.py
+++ taran.py
@@ -10,56 +10,27 @@ def genRandom():
 
 	return input
 
-#quick sort
-def quickSort(alist,rank):
-	quickSortSplit(alist,0,len(alist)-1,rank)
+def insertonSort(alist, index):
+	rank = [len(alist) for x in range(c-1)]
+	for i in range(len(alist)):
+		key = alist[i]
+		val = index[i]
+		j = i - 1 
+		while j >= 0 and alist[j] > key:
+			alist[j+1] = alist[j]
+			index[j+1] = index[j]
+			j -= 1
+		alist[j+1] = key
+		index[j+1] = val
 
-def quickSortSplit(alist,first,last,rank):
-	if first < last:
-		splitPoint = partition(alist,first,last,rank)
-
-		quickSortSplit(alist,first,splitPoint - 1,rank)
-		quickSortSplit(alist,splitPoint + 1, last,rank)
-
-def partition(alist, first, last,rank):
-	
-	pivot = alist[first]		
-
-	left = first + 1
-	right = last
-
-	done = False
-
-	while not done:
-
-		while left <= right and alist[left] <= pivot:
-			left = left + 1
-
-		while alist[right] >= pivot and right >= left:
-			right = right - 1
-
-		if right < left:
-			done = True
-		else:
-			temp = alist[left]
-			alist[left] = alist[right]
-			alist[right] = temp
-
-			temp1 = rank[left]
-			rank[left] = rank[right]
-			rank[right] = temp1
-
-
-	temp = alist[first]
-	alist[first] = alist[right]
-	alist[right] = temp
-
-	temp1 = rank[first]
-	rank[first] = rank[right]
-	rank[right] = temp1
-
-	return right
-
+	ranking = len(alist)
+	for i in range(len(alist)-1):
+		ranking -= 1
+		if alist[i] == alist[i+1]:
+	 		rank[index[i+1]] = rank[index[i]]
+	 	else:
+	 		rank[index[i+1]] = ranking
+	return rank
 
 #get score
 def getScore(test):
@@ -68,35 +39,39 @@ def getScore(test):
 	#testCov = [[1.0,1.0,0.0],[0.0,1.0,1.0],[1.0,0.0,0.0],[0.0,0.0,1.0]]
 	
 	testCov = test
-   	s = len(testCov[0])
-   	t = len(testCov)
-	hue = [0 for x in range(s)]
-	sus = [0 for x in range(s)]
+	c = len(testCov[0]) #num of columns
+	r = len(testCov) #num of rows
+	hue = [0 for x in range(c-1)]
+	sus = [0 for x in range(c-1)]
 	#print(testCov)
 	
 	totalPassed = 0.0
 	totalFailed = 0.0
 
-	for x in range(t):
-		if testCov[x][s-1] == 0.0:
+	for x in range(r):
+		if testCov[x][c-1] == 0:
 			totalPassed += 1
-	totalFailed = t - totalPassed
+	totalFailed = r - totalPassed
 
-	for x in range(s):
+	for x in range(c-1):
 		passed = 0.0
 		failed = 0.0
-		for y in range(t):
-			if testCov[y][x] == 1 and testCov[y][s-1] == 0:
-				passed +=1
-			if testCov[y][x] == 1 and testCov[y][s-1] == 1:
-				failed +=1
+		for y in range(r):
+			if testCov[y][x] == 1: #covered
+				if testCov[y][c-1] == 0:#passed
+					passed += 1
+				else:#failed
+					failed += 1
 		testZero = passed*totalFailed+failed*totalPassed
-		if 	totalPassed == 0 or totalFailed == 0 or testZero == 0:
-			hue[x] = 0
-			sus[x] = 1
+		if passed == 0 and failed == 0 or totalFailed == 0:
+			hue[x] = 1.0
+			sus[x] = 0.0
+		elif testZero == 0 or totalPassed == 0:
+			hue[x] = 0.0
+			sus[x] = 1.0
 		else:
 			hue[x] = (passed/totalPassed)/(passed/totalPassed+failed/totalFailed)
-			sus[x] = 1 - hue[x]
+			sus[x] = 1.0 - hue[x]
 
 
 	return sus,hue
@@ -110,15 +85,20 @@ test = [
     [1,1,1,1,0,1,0,0,0,0,0,0,1,0],
     [1,1,1,1,0,1,1,0,0,0,0,0,1,1],
     ]
-s = len(test[0])
+c = len(test[0])
 sus,hue = getScore(test)
-rank = [x for x in range(s)]
+index = [x for x in range(c-1)]
 
 
 sorted = list(sus)
-quickSort(sorted,rank)
+rank = insertonSort(sorted,index)
+# for x in range(c-1,0,-1):
+# 	print("x is :",x)
+# 	if x > 0:
+# 		if sorted[x-1] == sorted[x-2]:
+# 			rank[x-2] = rank[x-1]
 
 #rank will record index of the corresponding value in sorted
-print sus
 print sorted
-print rank
+#indexes corresponding to statement no.
+print ("rank is :",rank)
