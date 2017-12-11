743ddaf9a62b68dd906666ad473aaf8e446a2fb3
Jerry-zhk <jchong0618@gmail.com>
2017-12-11 15:31:34 +0800

    testcases

---
+++ gen_testcases.py
@@ -0,0 +1,18 @@
+import random
+def generate(folder_name = 'tc'):
+	for i in range(1000):
+		with open(folder_name + '/testcases_' + str(i) + '.txt', 'w') as f:
+			testcase = []
+			n = random.randint(1, 101)
+			for j in range(n):
+				val = random.randint(1, 1001)
+				testcase.append(val)
+			origin_list_str = ",".join(str(x) for x in testcase)
+			testcase.sort()
+			sorted_list_str = ",".join(str(x) for x in testcase)
+			f.write(origin_list_str + ' ' + sorted_list_str + '\n')
+	print('Generated testcases.txt')
+
+
+if __name__ == "__main__":
+	generate()
--- parse_coverages.py
+++ parse_coverages.py
@@ -2,33 +2,27 @@ from os import listdir
 from os.path import isfile, join
 import xml.etree.ElementTree as ET
 
+# folder: the name of folder contains coverage xml files
+# program_name: the name of the python program, i.e. XXXX.py
 def get_tests_matrix(folder, program_name):
 	tests_matrix = [];
 
 	for f in [f for f in listdir(folder) if isfile(join(folder, f))]:
-		print(f)
 		if not f.endswith('.xml'):
 			continue
 		test = []
 		tree = ET.parse(folder + '/' + f)
 
-
 		root = tree.getroot()
 		for pkg in root.findall('.//package'):
-		    print(pkg.tag)
-		    for clazz in pkg.findall('.//class[@filename="' + program_name + '"]'):
-		        print(clazz.tag)
-		        for line in clazz.findall('.//line'):
-		            print('line ' + line.get('number') + ' : ' + line.get('hits'))
-		            test.append(int(line.get('hits')))
+			for clazz in pkg.findall('.//class[@filename="' + program_name + '"]'):
+				for line in clazz.findall('.//line'):
+					test.append(int(line.get('hits')))
 		result = root.find('result').get('pass')
 		test.append(int(result))
 		tests_matrix.append(test)
-		print('result: ' + result)
 
-	print(tests_matrix)
 	return tests_matrix
 
 if __name__ == '__main__':
-	#get_tests_matrix('bubbleSort', 'bubbleSort.py')
 	get_tests_matrix('quickSort', 'quickSort.py')
