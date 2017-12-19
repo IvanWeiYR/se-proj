56ce711377cf396bb53d4b808a2e5e0433b29a64
Jerry-zhk <jchong0618@gmail.com>
2017-12-19 20:27:32 +0800

    clean up

--- insertionSort.py
+++
@@ -1,9 +0,0 @@
-def insertionSort(alist):
-	for i in range(len(alist) -1):
-		key = alist[i]
-		j = i - 1 
-		while j >= 0 and alist[j] > key:
-			alist[j+1] = alist[j]
-			j -= 1
-		alist[j+1] = key
-	return alist
